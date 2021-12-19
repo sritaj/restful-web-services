be95c6a599b082bca3a8d201319967a1bf6b02bf
Sritaj <sritajp@gmail.com>
2021-12-19 23:47:27 +0530

    Added API for path variable and updated methods name

--- src/main/java/com/sritaj/rest/webservices/controllers/TestControllers.java
+++ src/main/java/com/sritaj/rest/webservices/controllers/TestControllers.java
@@ -1,8 +1,8 @@
 package com.sritaj.rest.webservices.controllers;
 
 import com.sritaj.rest.webservices.beans.HelloWorld;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestControllers {
 
     @GetMapping(path = "/hello-world")
-    public String sampleTest(){
+    public String helloWorld(){
         return "Hello Test";
     }
 
     @GetMapping(path = "/hello-world-bean")
-    public HelloWorld sampleUserSpecidiedTest2(){
+    public HelloWorld helloWorldBean(){
         return new HelloWorld("Hello World -     Bean Test");
+    }
+
+    @GetMapping(path = "/hello-world-pathvariable/{text}")
+    public HelloWorld helloWorldPathVariable(@PathVariable String text){
+        return new HelloWorld(String.format("Hello World By - %s", text));
     }
 }
