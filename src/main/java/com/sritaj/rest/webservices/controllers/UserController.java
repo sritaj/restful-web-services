54234fbe741c682095f6dc7841b1ce357cecdc7a
Sritaj <sritajp@gmail.com>
2021-12-23 00:10:53 +0530

    Used Entitymodel from hateOS to send all users link on getUser API call

--- src/main/java/com/sritaj/rest/webservices/controllers/UserController.java
+++ src/main/java/com/sritaj/rest/webservices/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.sritaj.rest.webservices.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import com.sritaj.rest.webservices.beans.User;
 import com.sritaj.rest.webservices.dao.UserDaoService;
 import com.sritaj.rest.webservices.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -25,13 +29,16 @@ public class UserController {
     }
 
     @GetMapping(path = "getuser/{id}")
-    public User getUser(@PathVariable int id){
+    public EntityModel<User> getUser(@PathVariable int id){
         User user =  userDaoService.findUser(id);
         if(user == null){
             throw new UserNotFoundException("Specified user with id " + id + " is not found");
         }
 
-        return user;
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkToUsers  = linkTo(methodOn(this.getClass()).returnAllUsers());
+        model.add(linkToUsers.withRel("all-users"));
+        return model;
     }
 
     @PostMapping(path = "createuser")
