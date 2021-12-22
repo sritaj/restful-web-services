b5b6989fc3037d93b3fb5380aa39436996b26404
Sritaj <sritajp@gmail.com>
2021-12-23 00:36:09 +0530

    Added controller method API to retrieve value based on language context

--- src/main/java/com/sritaj/rest/webservices/controllers/TestControllers.java
+++ src/main/java/com/sritaj/rest/webservices/controllers/TestControllers.java
@@ -1,14 +1,20 @@
 package com.sritaj.rest.webservices.controllers;
 
 import com.sritaj.rest.webservices.beans.HelloWorld;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("")
 public class TestControllers {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -23,5 +29,16 @@ public class TestControllers {
     @GetMapping(path = "/hello-world-pathvariable/{text}")
     public HelloWorld helloWorldPathVariable(@PathVariable String text){
         return new HelloWorld(String.format("Hello World By - %s", text));
+    }
+
+    // Using Locale as attribute 
+//    @GetMapping(path = "/hello-world-internationalized")
+//    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required=false) Locale locale){
+//        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
+//    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, "Default message", LocaleContextHolder.getLocale());
     }
 }
