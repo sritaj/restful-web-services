0b393046b24603f7b0b21827d716f85cdff01470
Sritaj <sritajp@gmail.com>
2021-12-22 23:52:06 +0530

    Added validations for respective columns name and date

--- src/main/java/com/sritaj/rest/webservices/beans/User.java
+++ src/main/java/com/sritaj/rest/webservices/beans/User.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter
@@ -15,7 +17,9 @@ public class User {
 
     private Integer id;
 
+    @Size(min = 2, message = "User name should be min 2 characters")
     private String name;
 
+    @Past
     private Date dateOfBirth;
 }
